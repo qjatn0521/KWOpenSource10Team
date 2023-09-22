ca178e56dde132dc21d99769b584cc0dd8e787fb
qjatn0521 <qjatn0521@kw.ac.kr>
2023-09-22 23:54:17 +0900

    Feat : Add BottomNavagation
    바텀 네비게이션 클릭 시 프래그먼트 변경 기능 추가

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,62 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.example.myapplication.databinding.ActivityMainBinding;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    //바텀 네비게이션
+    private BottomNavigationView bottomNavigationView;
+    private FragmentManager fragmentManager;
+
+
+    Fragment fragmentAlarm;
+    Fragment fragmentNotification ;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        bottomNavigationView = findViewById(R.id.bottomNavigationView);
+        fragmentManager = getSupportFragmentManager();
+
+        loadFragment(new FragAlarm());
+
+        // 바텀 네비게이션 아이템 클릭 이벤트 처리
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(MenuItem item) {
+                Fragment fragment = null;
+                // 클릭된 아이템에 따라 프래그먼트를 변경합니다.
+                if (item.getItemId() == R.id.alarm) {
+                    fragment = new FragAlarm();
+                } else if (item.getItemId() == R.id.notification) {
+                    fragment = new FragNoti();
+                }
+
+                return loadFragment(fragment);
+            }
+        });
+    }
+
+    private boolean loadFragment(Fragment fragment) {
+        if (fragment != null) {
+            FragmentTransaction transaction = fragmentManager.beginTransaction();
+            transaction.replace(R.id.main_frame, fragment);
+            transaction.commit();
+            return true;
+        }
+        return false;
+    }
+}
