d551ed07ec6c91ece4814bfcad2650e40ac2806b
we_BS <qjatn0521@kw.ac.kr>
2023-10-18 21:06:25 +0900

    UI : 스포츠 팀 리사이클러뷰에 이미지 추가

--- app/src/main/java/com/example/myapplication/sports/adapter/TeamAdapter.java
+++ app/src/main/java/com/example/myapplication/sports/adapter/TeamAdapter.java
@@ -1,5 +1,7 @@
 package com.example.myapplication.sports.adapter;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,9 +14,17 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.example.myapplication.R;
 import com.example.myapplication.sports.model.Team;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
-
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
 public class TeamAdapter  extends RecyclerView.Adapter<TeamAdapter.MyViewHolder> {
 
     //리스트는 무조건 데이터를 필요로함
@@ -35,7 +45,11 @@ public class TeamAdapter  extends RecyclerView.Adapter<TeamAdapter.MyViewHolder>
     @Override
     public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
         Team data=items.get(position);
-        holder.setItem(data);
+        try {
+            holder.setItem(data);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
@@ -53,9 +67,22 @@ public class TeamAdapter  extends RecyclerView.Adapter<TeamAdapter.MyViewHolder>
             tvTitle=itemView.findViewById(R.id.tv_title);
             imageResource=itemView.findViewById(R.id.iv_img_resource);
         }
-        public void setItem(Team data){
+        public void setItem(Team data) throws IOException {
             tvTitle.setText(data.getName());
-            //imageResource.setImageResource(data.getLogo());
+            URL url = new URL(data.getLogo());
+
+            // web에서 이미지를 가져와 ImageView에 저장할 Bitmap을 만든다.
+            HttpURLConnection conn = (HttpURLConnection)url.openConnection();
+            conn.setDoInput(true); // 서버로부터 응답 수신
+            InputStream is;
+            try {
+                conn.connect(); //연결된 곳에 접속할 때 (connect() 호출해야 실제 통신 가능함)
+                is = conn.getInputStream(); //inputStream 값 가져오기
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+            Bitmap bitmap = BitmapFactory.decodeStream(is);
+            imageResource.setImageBitmap(bitmap);
         }
     }
 }
