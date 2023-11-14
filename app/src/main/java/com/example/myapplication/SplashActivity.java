4eca1bef5c33ee36508272084a3857f3d51f38e6
qjatn0521 <qjatn0521@kw.ac.kr>
2023-11-15 00:44:19 +0900

    [Design] 전체적인 디자인 수정

--- app/src/main/java/com/example/myapplication/SplashActivity.java
+++ app/src/main/java/com/example/myapplication/SplashActivity.java
@@ -31,6 +31,6 @@ public class SplashActivity extends AppCompatActivity {
                 startActivity(intent);
                 finish();
             }
-        },3000);
+        },2800);
     }
 }
