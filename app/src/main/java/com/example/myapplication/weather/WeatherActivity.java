cfb585664c5ae2b61ea7eb4ed73150250c055a6b
Heo <nicehcy2@naver.com>
2023-11-15 18:14:41 +0900

    20231115

--- app/src/main/java/com/example/myapplication/weather/WeatherActivity.java
+++ app/src/main/java/com/example/myapplication/weather/WeatherActivity.java
@@ -1,34 +1,49 @@
 package com.example.myapplication.weather;
 
-import static android.app.PendingIntent.getActivity;
-
-import android.app.Activity;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
-import android.os.Message;
 import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+
 import com.example.myapplication.R;
 import com.example.myapplication.weather.api.UltraSrtNcstAPI;
 import com.example.myapplication.weather.api.VillageFcstAPI;
+import com.example.myapplication.weather.location.GPSLocation;
 
 import java.util.List;
-import java.util.concurrent.atomic.AtomicReference;
 
 public class WeatherActivity extends AppCompatActivity {
+    private GPSLocation location;
+    private static final int MIN_TIME_BW_UPDATES = 1000; // 1초마다
+    private static final float MIN_DISTANCE_CHANGE_FOR_UPDATES = 10.0f; // 10 미터마다
+
+    private LocationManager locationManager;
+    private LocationListener locationListener;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.frag_weather);
 
-        UltraSrtNcstAPI ulweather = new UltraSrtNcstAPI("20231109", "0000", "55", "127");
-        VillageFcstAPI  viweather = new VillageFcstAPI("20231108", "2300", "55", "127");
+        UltraSrtNcstAPI ulweather = new UltraSrtNcstAPI("20231115", "0000", "55", "127");
+        VillageFcstAPI viweather = new VillageFcstAPI("20231114", "2300", "55", "127");
 
         TextView temperature = findViewById(R.id.temperature);
         TextView highTemp = findViewById(R.id.HighTemp);
 
-        // API를 받아오는 Thread
+        /** 현재 위치를 받아오는 코드 */
+        location = new GPSLocation(WeatherActivity.this);
+
+        double latitude = location.getLatitude();
+        double longitude = location.getLongitude();
+
+        System.out.println("latitude = " + latitude);
+        System.out.println("longitude = " + longitude);
+
+        /** API를 받아오는 Thread */
         new Thread(new Runnable() {
             @Override
             public void run() {
