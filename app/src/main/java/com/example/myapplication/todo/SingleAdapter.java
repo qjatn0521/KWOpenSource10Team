d328be029a3d5c896d47c1d80070f20c294abce8
Daehyun Choi <gkdiseogus98@naver.com>
2023-11-15 18:49:57 +0900

    feat:할 일 입출력 구현 전

--- app/src/main/java/com/example/myapplication/todo/SingleAdapter.java
+++ app/src/main/java/com/example/myapplication/todo/SingleAdapter.java
@@ -7,44 +7,53 @@ import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
 import com.example.myapplication.R;
+import com.example.myapplication.sports.adapter.TeamAdapter;
 
 import java.util.ArrayList;
 import java.util.List;
-public class SingleAdapter extends BaseAdapter{
+public class SingleAdapter extends RecyclerView.Adapter<SingleAdapter.MyViewHolder>{
     private static final String TAG="SingleAdapter";
 
-    private List<String> items=new ArrayList<>();
+    private List<TodoInput> items=new ArrayList<>();
 
-    public void addItems(List<String> items){
-        this.items=items;
+    public void addItem(TodoInput todo){
+        items.add(todo);
     }
 
-    @Override
-    public int getCount(){
-        Log.d(TAG, "getCount: ");
+    @NonNull
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType){
+        Log.d(TAG, "onCreateViewHolder: ");
+        LayoutInflater inflater = LayoutInflater.from(parent.getContext());
+        View view=inflater.inflate(R.layout.todo_item, parent, false);
+        return new MyViewHolder(view);
+    }
+
+    public void onBindViewHolder(@NonNull MyViewHolder holder, int position){
+        Log.d(TAG, "onBindViewHolder: "+position);
+        TodoInput todo=items.get(position);
+        holder.setItem(todo);
+    }
+
+    public int getItemCount(){
+        Log.d(TAG, "getItemCount: ");
         return items.size();
     }
 
-    @Override
-    public Object getItem(int position){
-        Log.d(TAG, "getItem: ");
-        return items.get(position);
-    }
+    public static class MyViewHolder extends RecyclerView.ViewHolder{
+        private TextView todo_list;
 
-    @Override
-    public long getItemId(int position){
-        Log.d(TAG, "getItemId: ");
-        return position;
-    }
+        public MyViewHolder(@NonNull View itemView){
+            super(itemView);
 
-    @Override
-    public View getView(int position, View convertView, ViewGroup parent){
-        Log.d(TAG, "getView: "+position);
-        LayoutInflater inflater=LayoutInflater.from(parent.getContext());
-        View itemView=inflater.inflate(R.layout.todo_item,parent, false);
-        TextView todo=itemView.findViewById(R.id.todo_single);
-        todo.setText(getItem(position).toString());
-        return itemView;
+            todo_list=itemView.findViewById(R.id.todo_single);
+        }
+        public void setItem(TodoInput todo){
+            Log.d(TAG, "MyViewHolder: ");
+            todo_list.setText(todo.getTodo_input());
+        }
     }
 }
--- app/src/main/java/com/example/myapplication/todo/TodoActivity.java
+++ app/src/main/java/com/example/myapplication/todo/TodoActivity.java
@@ -1,37 +1,30 @@
 package com.example.myapplication.todo;
 
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Context;
-import android.opengl.EGLExt;
-import android.os.Bundle;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
-
 import com.example.myapplication.R;
-
-import java.util.Arrays;
-import java.util.List;
 
 public class TodoActivity extends AppCompatActivity{
 
     private static final String TAG="Todo_Activity";
-    private Context mContext=TodoActivity.this;
-    private ListView listView;
-    private SingleAdapter adapter;
-
-    List<String> items=Arrays.asList("할 일 1", "할 일 2", "할 일 3", "할 일 4", "할 일 5");
+    private RecyclerView rvTodo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_todo);
 
-        adapter=new SingleAdapter();
-        listView=findViewById(R.id.list_view);
+        rvTodo=findViewById(R.id.rvTodo);
 
-        adapter.addItems(items);
+        SingleAdapter adapter=new SingleAdapter();
+        adapter.addItem(new TodoInput("input"));
 
-        listView.setAdapter(adapter);
+        RecyclerView.LayoutManager layoutManager=new LinearLayoutManager(this);
+        rvTodo.setLayoutManager(layoutManager);
+        rvTodo.setAdapter(adapter);
     }
 }
