c547e2638467c6c6c43b303c22c28051f8bff816
Daehyun Choi <gkdiseogus98@naver.com>
2023-11-19 15:22:31 +0900

    feat:할 일 입출력 구현

--- app/src/main/java/com/example/myapplication/todo/TodoActivity.java
+++ app/src/main/java/com/example/myapplication/todo/TodoActivity.java
@@ -1,30 +1,50 @@
 package com.example.myapplication.todo;
 
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import android.os.Bundle;
-import androidx.appcompat.app.AppCompatActivity;
-
 import com.example.myapplication.R;
 
-public class TodoActivity extends AppCompatActivity{
+import java.util.ArrayList;
 
-    private static final String TAG="Todo_Activity";
-    private RecyclerView rvTodo;
+public class TodoActivity extends AppCompatActivity {
+
+    private ArrayList<String> todoList;
+    private RecyclerView recyclerView;
+    private TodoAdapter adapter;
+    private EditText todoInput;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState){
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_todo);
 
-        rvTodo=findViewById(R.id.rvTodo);
+        //할 일 목록 및 recyclerview 초기화
+        todoList = new ArrayList<>();
+        recyclerView = findViewById(R.id.todoRecyclerView);
+        adapter = new TodoAdapter(todoList);
 
-        SingleAdapter adapter=new SingleAdapter();
-        adapter.addItem(new TodoInput("input"));
+        //레이아웃 매니저 및 어댑터 설정
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setAdapter(adapter);
 
-        RecyclerView.LayoutManager layoutManager=new LinearLayoutManager(this);
-        rvTodo.setLayoutManager(layoutManager);
-        rvTodo.setAdapter(adapter);
+        //EditText 초기화
+        todoInput = findViewById(R.id.todoInput);
+    }
+
+    //버튼 기능
+    public void addTask(View view) {
+        String task = todoInput.getText().toString(); //EditText에서 입력 가져옴
+        if (!task.isEmpty()) {
+            todoList.add(task); //목록에 추가
+            adapter.notifyDataSetChanged();
+            todoInput.getText().clear();
+        }
     }
 }
